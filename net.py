774b4a7d4044589c2fce74fa409f57c546f5a11f
linkinpark213 <linkinpark213@outlook.com>
2019-03-04 23:24:36 +0900

    Finished version

--- net.py
+++ net.py
@@ -10,21 +10,34 @@ class SurnameLSTM(nn.Module):
 
         # まず　ASCIIコード　を　ベクトル　に　しましょう！
         # TODO: 1. Map the ASCII codes to embeddings.
+        self.embedding = nn.Embedding(128, 128)
 
         # LSTM層？
         # TODO: 2. Add the LSTM layer.
+        self.lstm = nn.LSTM(128, 32, 1)
 
         # Dense層？
         # TODO: 3. Add some dense layers and activation functions.
+        self.fc1 = nn.Linear(32, 256)
+        self.sigmoid = nn.Sigmoid()
+        self.fc2 = nn.Linear(256, 3)
 
         # Dropout層？
         # TODO: 4. Add a dropout layer.
+        self.dropout = nn.Dropout(p=0.2)
 
         # Softmax　クラシフィケーション層？
         # TODO: 5. Add a softmax layer for classification.
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, input: torch.Tensor):
         # フォワードプロパゲーション　を　しましょう！
         # TODO: Implement the forward-propagation.
-        
-        pass
+        embedded_input = self.embedding(input.transpose(0, 1).type(torch.LongTensor))
+        output, (ht, ct) = self.lstm(embedded_input)
+        out = self.fc1(ht[0])
+        out = self.sigmoid(out)
+        out = self.fc2(out)
+        out = self.dropout(out)
+        out = self.softmax(out)
+        return out
--- train.py
+++ train.py
@@ -10,43 +10,78 @@ from data import SurnameDataset
 from tensorboardX import SummaryWriter
 
 if __name__ == '__main__':
-    # データ　ローダー　を 準備しましょう！
+    # データ　ローダー　を 準備しよう！
     # TODO: Build data loader.
-    train_dataset = None
-    train_data_loader = None
-    val_dataset = None
-    val_data_loader = None
+    train_dataset = SurnameDataset(subset='train')
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle=True)
+    val_dataset = SurnameDataset(subset='val')
+    val_data_loader = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=True)
+
+    # Generate a name for the run
+    run_name = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+    if not os.path.isdir('./log/'):
+        os.mkdir('./log/')
+    log_path = './log/{}/'.format(run_name)
+    os.mkdir(log_path)
+    writer = SummaryWriter(log_path)
 
     # モデル　と　オプティマイザ　は何でしょうか？
     # TODO: Build the network, loss and optimizer.
-    net = None
-    loss = None
-    optimizer = None
+    net = SurnameLSTM()
+    criterion = nn.MSELoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.1)
 
     # トレニング　１００エポック　を しましょう！
     # TODO: Train the model for 100 epochs.
+    global_step = 0
     for i in range(100):
         for j, sample in enumerate(train_data_loader):
             # 勾配クリア　を　忘れないで〜
             # TODO: Remember to clear the gradient every time.
+            net.zero_grad()
 
             # プリディクション　と　コストの計算　を　しましょう！
             # TODO: Prediction and loss calculation.
+            output = net(sample['values'])
+            loss = criterion(output, sample['label'])
 
             # 精度　を　計算しませんか？
             # TODO: Calculate accuracy for the whole batch.
+            pred = np.argmax(output.detach().numpy(), axis=1)
+            gt = np.argmax(sample['label'].numpy(), axis=1)
+            accuracy = np.average(np.where(pred == gt, 1, 0))
 
+            print('Epoch {}, Iteration {}, Loss = {:.3f}, Accuracy = {:.1f} %'.format(i + 1, j + 1, loss,
+                                                                                      accuracy * 100))
             # 最後に、バックプロパゲーション　を　しましょう！
             # TODO: Finally, time for back-propagation.
+            loss.backward()
+            optimizer.step()
 
             # バリデーション　を　しませんか？
             # TODO: Do validation every 10 iterations.
+            if global_step % 10 == 0:
+                sample = iter(val_data_loader).__next__()
+
+                output = net(sample['values'])
+                loss = criterion(output, sample['label'])
+
+                pred = np.argmax(output.detach().numpy(), axis=1)
+                gt = np.argmax(sample['label'].numpy(), axis=1)
+                accuracy = np.average(np.where(pred == gt, 1, 0))
+
+                print('Validation Loss = {:.3f}, Accuracy = {:.1f} %'.format(loss, accuracy * 100))
 
             # TensorboardX で　ロッグ　を　しませんか？
             # TODO: Log the loss values and accuracies with TensorboardX.
-
-
-            pass
+            global_step += 1
+            writer.add_scalar('loss', loss, global_step=global_step)
+            writer.add_scalar('accuracy', accuracy, global_step=global_step)
 
     # モデル　は　まだ使うつもりですが。。。
     # TODO: Save log and weights.
+    writer.close()
+    print('Training log saved to {}'.format(log_path))
+    state_dict = net.state_dict()
+    torch.save(state_dict, 'model.pth')
+    print('Model weights saved to ./model.pth')
